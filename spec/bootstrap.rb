df3a8344afaad2b5957344a201253022b591f350
Ben Batschelet <ben@codeup.com>
2016-02-02 14:17:52 -0600

    Initial collection of tasks and specs

--- spec/bootstrap.rb
+++ spec/bootstrap.rb
@@ -9,10 +9,21 @@ set :host,        options[:host_name]
 set :ssh_options, options
 
 # Disable sudo
-set :disable_sudo, true
+set :disable_sudo, false
 
 # Set environment variables
 # set :env, :LANG => 'C', :LC_MESSAGES => 'C'
 
 # Set PATH
 set :path, '/sbin:/usr/local/sbin:/usr/local/bin:$PATH'
+
+shared_examples "nginx::config" do
+  describe command("nginx -t") do
+    # stderr?? Wtf nginx.
+    its(:stderr) { should match /configuration file \/etc\/nginx\/nginx\.conf syntax is ok/ }
+    its(:stderr) { should match /configuration file \/etc\/nginx\/nginx\.conf test is successful/ }
+
+    its(:exit_status) { should eq 0 }
+  end
+end
+
---
+++ spec/node-spec.rb
@@ -0,0 +1,30 @@
+require_relative "lib/ansible_helper"
+require_relative "bootstrap"
+
+RSpec.configure do |config|
+  config.before :suite do
+    AnsibleHelper.instance.playbook "playbooks/node-playbook.yml", { copy_appjs: true, node_version: "v5.5.0" }
+  end
+end
+
+describe "Nginx config should be valid" do
+  include_examples "nginx::config"
+end
+
+describe command(". /etc/profile.d/nvm.sh && nvm run node -e \"console.log('node-installed');\"") do
+  its(:stdout) { should match /node-installed/ }
+
+  its(:exit_status) { should eq 0 }
+end
+
+describe command(". /etc/profile.d/nvm.sh && nvm run node --version") do
+  its(:stdout) { should match /^v5\.5\.0$/ }
+
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('printf "GET / HTTP/1.1\nHost: node-test.dev\n\n" | nc 127.0.0.1 80') do
+  its(:stdout) { should match /^+HTTP\/1\.1 200 OK$/ }
+
+  its(:stdout) { should match /Phusion Passenger is serving Node.js v5\.5\.0 code on node-test\.dev/ }
+end
