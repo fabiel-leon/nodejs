910b6ec58d79fe6943b830e68f37e8dc5feab005
Ben Batschelet <bartleby84@gmail.com>
2018-01-22 11:00:44 -0600

    Update spec

--- spec/node-spec.rb
+++ spec/node-spec.rb
@@ -1,24 +1,26 @@
-require_relative "lib/ansible_helper"
 require_relative "bootstrap"
 
 RSpec.configure do |config|
   config.before :suite do
-    AnsibleHelper.instance.playbook "playbooks/node-playbook.yml", { copy_appjs: true, node_version: "v5.5.0" }
+    AnsibleHelper.playbook("playbooks/node-playbook.yml", ENV["TARGET_HOST"], {
+      copy_appjs: true,
+      node_version: "v9.4.0"
+    })
   end
 end
 
-describe "Nginx config should be valid" do
-  include_examples "nginx::config"
+describe "Nginx" do
+  include_examples "nginx"
 end
 
-describe command("nvm run 5.5.0 -e \"console.log('node-installed');\"") do
+describe command("nvm run 9.4.0 -e \"console.log('node-installed');\"") do
   its(:stdout) { should match /node-installed/ }
 
   its(:exit_status) { should eq 0 }
 end
 
-describe command("nvm run 5.5.0 --version") do
-  its(:stdout) { should match /^v5\.5\.0$/ }
+describe command("nvm run 9.4.0 --version") do
+  its(:stdout) { should match /^v9\.4\.0$/ }
 
   its(:exit_status) { should eq 0 }
 end
@@ -26,5 +28,5 @@ end
 describe command('curl -i node-test.dev') do
   its(:stdout) { should match /^HTTP\/1\.1 200 OK$/ }
 
-  its(:stdout) { should match /Phusion Passenger is serving Node.js v5\.5\.0 code on node-test\.dev/ }
+  its(:stdout) { should match /Phusion Passenger is serving Node.js v9\.4\.0 code on node-test\.dev/ }
 end
